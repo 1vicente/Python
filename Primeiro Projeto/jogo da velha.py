8c8a80af743a4942ff51ce2df72816f6b8dc672a
Vicente Siegel <visiegel1@gmail.com>
2020-04-14 11:13:50 -0300

    ajuste do código com o flask8

--- Primeiro Projeto/jogo da velha.py
+++ Primeiro Projeto/jogo da velha.py
@@ -1,6 +1,7 @@
-lista=[[''],['0', '1', '1','1'],['0', '2', '2','2'],['0', '3', '3','3']]
+lista = [[''], ['0', '1', '1', '1'], ['0', '2', '2', '2'], ['0', '3', '3', '3']]
 
-### Função que define o tabuleiro
+
+# Função que define o tabuleiro
 def tabuleiro():
     print("""
          1       2       3
@@ -9,24 +10,26 @@ def tabuleiro():
     2    %s   |   %s   |   %s   \n
         -------------------\n
     3    %s   |   %s   |   %s   \n
-          """ %(lista[1][1],lista[1][2],lista[1][3],lista[2][1],lista[2][2],lista[2][3],lista[3][1],lista[3][2],lista[3][3]))
+          """ % (lista[1][1], lista[1][2], lista[1][3], lista[2][1], lista[2][2], lista[2][3], lista[3][1], lista[3][2], lista[3][3]))
 
-### Função que define as condições de vitória por linha horizontal, vertical ou diagonal
+
+# Função que define as condições de vitória por linha horizontal, vertical ou diagonal
 def vitoria():
     if len(set(lista[1])) == 2:
-        print('O jogador %s ganhou.' %(lista[1][1]))
-    elif lista[1][1] == lista[2][1] ==lista[3][1]:
-        print('O jogador %s ganhou.' %(lista[1][1]))
-    elif lista[1][2] == lista[2][2] ==lista[3][2]:
-        print('O jogador %s ganhou.' %(lista[1][2]))
-    elif lista[1][3] == lista[2][3] ==lista[3][3]:
-        print('O jogador %s ganhou.' %(lista[1][3]))
-    elif lista[1][1] == lista[2][2] ==lista[3][3]:
-        print('O jogador %s ganhou.' %(lista[1][1]))
-    elif lista[3][1] == lista[2][2] ==lista[1][3]:
-        print('O jogador %s ganhou.' %(lista[3][1]))
+        print('O jogador %s ganhou.' % (lista[1][1]))
+    elif lista[1][1] == lista[2][1] == lista[3][1]:
+        print('O jogador %s ganhou.' % (lista[1][1]))
+    elif lista[1][2] == lista[2][2] == lista[3][2]:
+        print('O jogador %s ganhou.' % (lista[1][2]))
+    elif lista[1][3] == lista[2][3] == lista[3][3]:
+        print('O jogador %s ganhou.' % (lista[1][3]))
+    elif lista[1][1] == lista[2][2] == lista[3][3]:
+        print('O jogador %s ganhou.' % (lista[1][1]))
+    elif lista[3][1] == lista[2][2] == lista[1][3]:
+        print('O jogador %s ganhou.' % (lista[3][1]))
 
-### Função que decide de quem é a vez de jogar
+
+# Função que decide de quem é a vez de jogar
 def jogada():
     count = 1
     while count <= 9:
@@ -47,11 +50,11 @@ def jogada():
             vitoria()
             count += 1
 
-### Início do cógido onde as funções são chamadas
-again='yes'
 
+# Início do cógido onde as funções são chamadas
+again = 'yes'
 while again == 'yes':
     tabuleiro()
     jogada()
     again = input("""Deseja jogar novamente?:\n""")
-    lista=[[''],['0', '1', '1','1'],['0', '2', '2','2'],['0', '3', '3','3']]
+    lista = [[''], ['0', '1', '1', '1'], ['0', '2', '2', '2'], ['0', '3', '3', '3']]
